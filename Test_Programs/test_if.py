ad14b7f54d8a4c2e26b7dc9346217674d659daab
ssdas-ubuntu <ssdas.in@gmail.com>
2023-12-29 13:31:06 +0530

    Added If else program

---
+++ Test_Programs/test_if.py
@@ -0,0 +1,13 @@
+# This program tests the if condition in python
+print ("This program is written to show the execution of if statement")
+
+value = int(input("Enter a interger value: "))
+
+if value > 0 :
+    print("You have entered a positive number")
+
+elif value < 0:
+    print("You have entered a negative number")
+else:
+    print("You have entered 0") 
+
